2d4acba94030df880649012dea2eda221188297a
Juan Camilo Clement Santacruz <juancamiloclement@gmail.com>
2023-11-08 14:45:47 -0500

    docs partially

---
+++ lib/live_web/controllers/page_html.ex
@@ -0,0 +1,5 @@
+defmodule LiveWeb.PageHTML do
+  use LiveWeb, :html
+
+  embed_templates "page_html/*"
+end
---
+++ test/live_web/controllers/error_html_test.exs
@@ -0,0 +1,14 @@
+defmodule LiveWeb.ErrorHTMLTest do
+  use LiveWeb.ConnCase, async: true
+
+  # Bring render_to_string/4 for testing custom views
+  import Phoenix.Template
+
+  test "renders 404.html" do
+    assert render_to_string(LiveWeb.ErrorHTML, "404", "html", []) == "Not Found"
+  end
+
+  test "renders 500.html" do
+    assert render_to_string(LiveWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+  end
+end
